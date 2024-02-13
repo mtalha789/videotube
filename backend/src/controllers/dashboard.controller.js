3698449f6fb210fb01b6414dd5b4d9624abafdef
Talha <mt452395@gmail.com>
2024-02-13 22:38:41 +0500

    all controllers completed

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -53,7 +53,17 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
-    
+    const videos = await Video.aggregate([
+        {
+            $match:{
+                owner : new mongoose.Types.ObjectId(req.user?._id)
+            }
+        }
+    ])
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,videos,"fetched videos successfully"))
 })
 
 export {
